f41f68d2e85544d0f2410d222109d4009426fa44
sanam-umer <81857355+sanam-umer@users.noreply.github.com>
2021-08-01 13:48:07 +0530

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  
+
+
+  constructor( private formBuilder: FormBuilder,
+    private route: ActivatedRoute, private router: Router, ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+}
+
